0015fc9944548abf5068612b08318604328212a9
Da11oS <4eduma4@gmail.com>
2021-03-30 21:08:04 +0300

    Change script2

--- Assets/Scripts/Script2.cs
+++ Assets/Scripts/Script2.cs
@@ -7,7 +7,7 @@ public class Script2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Scrip2 test");
     }
 
     // Update is called once per frame
