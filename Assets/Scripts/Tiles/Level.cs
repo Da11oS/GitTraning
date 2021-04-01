bfeebefa79548d7d58bfbc0200b23075d58f3808
Da11oS <4eduma4@gmail.com>
2021-04-01 21:37:31 +0300

    Added tiles

---
+++ Assets/Scripts/Tiles/Level.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public List<Tile> Tiles;
+    public Tile CurrentTile;
+    static public Level Instance;
+
+    void Start()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        CurrentTile.Enter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject Pass1;
+    public GameObject Pass2;
+    public bool IsGoalAchived;
+    
+    public void Enter()
+    {
+
+    }
+    public void Exit()
+    {
+
+    }
+}
