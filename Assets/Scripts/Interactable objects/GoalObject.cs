3e5f860d19831f5e25543051d22aaca2c1ba7f59
Da11oS <4eduma4@gmail.com>
2021-04-02 20:53:38 +0300

    Added interactable objects scripts

---
+++ Assets/Scripts/Interactable objects/GoalObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalObject : MonoBehaviour, IObject
+{
+    public void Look()
+    {
+
+    }
+    public void Interact()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interactable objects/ItemObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour, IObject
+{
+    public void Look()
+    {
+
+    }
+    public void Interact()
+    {
+
+    }
+}
