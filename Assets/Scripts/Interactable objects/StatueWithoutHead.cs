14859edc336d39187b395087a8c930d4849ede61
Da11oS <4eduma4@gmail.com>
2021-04-04 00:26:35 +0300

    I crashed

---
+++ Assets/Scripts/Interactable objects/StatueWithoutHead.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueWihtoutHead : ItemInteractObject
+{
+    override public void Interact()
+    {
+        base.Interact();
+        if (IsActive)
+        {
+            Level.Instance.CurrentTile.IsGoalAchived = true;
+        }
+    }
+}
